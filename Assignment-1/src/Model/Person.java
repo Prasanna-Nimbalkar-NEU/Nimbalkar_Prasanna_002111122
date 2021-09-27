39d57ab7634001dc8218149026ac755ef6138945
Prasanna Nimbalkar <nimbalkar.p@northeastern.edu>
2021-09-26 23:43:35 -0400

    final commit

---
+++ Assignment-1/src/Model/Person.java
@@ -0,0 +1,188 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Praz
+ */
+public class Person {
+    
+    private String Name;
+    private String GeoArea;
+    private String DOB;
+    private int TelNum;
+    private int FaxNum;
+    private String eMail;
+    private String SSN;
+    private int MRN;
+    private int hpbNum;
+    private int bAccNum;
+    private String licenseNum;
+    private String vIdNum;
+    private String dIdNum;
+    private String linkedIn;
+    private String IPA;
+    private String BioId;
+    private long pic;
+    private String UID;
+    
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getGeoArea() {
+        return GeoArea;
+    }
+
+    public void setGeoArea(String GeoArea) {
+        this.GeoArea = GeoArea;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public int getTelNum() {
+        return TelNum;
+    }
+
+    public void setTelNum(int TelNum) {
+        this.TelNum = TelNum;
+    }
+
+    public int getFaxNum() {
+        return FaxNum;
+    }
+
+    public void setFaxNum(int FaxNum) {
+        this.FaxNum = FaxNum;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public int getMRN() {
+        return MRN;
+    }
+
+    public void setMRN(int MRN) {
+        this.MRN = MRN;
+    }
+
+    public int gethpbNum() {
+        return hpbNum;
+    }
+
+    public void sethpbNum(int hpbNum) {
+        this.hpbNum = hpbNum;
+    }
+
+    public int getbAccNum() {
+        return bAccNum;
+    }
+
+    public void setbAccNum(int bAccNum) {
+        this.bAccNum = bAccNum;
+    }
+
+    public String getlicenseNum() {
+        return licenseNum;
+    }
+
+    public void setlicenseNum(String licenseNum) {
+        this.licenseNum = licenseNum;
+    }
+
+    public String getvIdNum() {
+        return vIdNum;
+    }
+
+    public void setvIdNum(String vIdNum) {
+        this.vIdNum = vIdNum;
+    }
+
+    public String getdIdNum() {
+        return dIdNum;
+    }
+
+    public void setdIdNum(String dIdNum) {
+        this.dIdNum = dIdNum;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+    public String getIPA() {
+        return IPA;
+    }
+
+    public void setIPA(String IPA) {
+        this.IPA = IPA;
+    }
+
+    public String getBioId() {
+        return BioId;
+    }
+
+    public void setBioId(String BioId) {
+        this.BioId = BioId;
+    }
+
+    public long getPic() {
+        return pic;
+    }
+
+    public void setPic(long pic) {
+        this.pic = pic;
+    }
+
+    public String getUID() {
+        return UID;
+    }
+
+    public void setUID(String UID) {
+        this.UID = UID;
+    }
+
+    public String getBiometricsImage() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setBiometricsImage(String selectedImagePath) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Nimbalkar_Prasanna_1439136
+
+Assignment 1
+
